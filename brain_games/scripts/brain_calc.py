9490ba0e928ab59358462875aeaf2b7562dd9f31
Дмитрий Чачимин <chachimin@gmail.com>
2024-04-09 22:59:59 +0300

    step 6 brain_calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from random import randint, choice
+import prompt
+
+
+def main():
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    print('What is the result of the expression?')
+    count = 0
+    while count < 3:
+        oper_1 = randint(0, 25)
+        oper_2 = randint(0, 25)
+        operation = choice(['+', '-', '*'])
+        match operation:
+            case '+':
+                cor_answ = oper_1 + oper_2
+            case '-':
+                cor_answ = oper_1 - oper_2
+            case '*':
+                cor_answ = oper_1 * oper_2
+        print(f'Question: {oper_1} {operation} {oper_2}')
+        answ = prompt.string('Your answer: ')
+        if int(answ) == cor_answ:
+            print('Correct!')
+            count += 1
+        else:
+            print(f"'{answ}'is wrong answer ;(. Correct answer was: '{cor_answ}'.")
+            print(f"Let`s try again, {name}!")
+            count = 0
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == '__main__':
+    main()
