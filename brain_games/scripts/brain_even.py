79c57b67ae7eb8c543dbe1ea34d87d40b84813c6
chachimin <Chachimin.DV@vibors.ru>
2024-04-11 13:22:19 +0300

    update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,26 +3,39 @@ from random import randrange
 import prompt
 
 
+def welcome_user():
+    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
+    print(f'Hello, {name}!\nAnswer "yes" if the number is even, otherwise answer "no".')
+    return name
+
+
+def is_even(num):
+    if num % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
 def main():
+<<<<<<< HEAD
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
+=======
+    name = welcome_user()
+>>>>>>> fde2fac (update brain_even.py)
     count = 0
     while count < 3:
         questing_number = randrange(100)
-        print(f'Question: {questing_number}')
-        answ = prompt.string('Your answer: ')
-        corr_answ = 'yes' if questing_number % 2 == 0 else 'no'
-        rule_1 = questing_number % 2 == 0 and answ == 'no'
-        rule_2 = questing_number % 2 != 0 and answ == 'yes'
-        rule_3 = answ not in ('no', 'yes')
-        if rule_1 or rule_2 or rule_3:
-            print(f"'{answ}' is wrong answer ;(. Correct answer '{corr_answ}'")
-            count = 0
-        else:
+        answ = prompt.string(f'Question: {questing_number}\nYour answer: ')
+        cor_answ = is_even(questing_number)
+        if answ == cor_answ:
             print('Correct!')
             count += 1
+        else:
+            print(f"'{answ}' is wrong answer ;(. Correct answer '{cor_answ}'")
+            count = 0
     print(f'Congratulations, {name}!')
 
 
