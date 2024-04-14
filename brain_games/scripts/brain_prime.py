6cf41ed846eec57fbca3ba9a1ffc12831be15aaa
Дмитрий Чачимин <chachimin@gmail.com>
2024-04-14 15:44:46 +0300

    исправлена ошибка проверки числа на простоту

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -12,8 +12,8 @@ def is_prime(number):
     for i in range(2, (number // 2) + 1):
         if number % i == 0:
             return 'no'
-        else:
-            return 'yes'
+    else:
+        return 'yes'
 
 
 def get_user_name():
